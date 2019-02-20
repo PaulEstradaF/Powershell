caefd1181d8802cf4b037a1c651b97c17e7d3ea1
Paul <ansibleoverride@gmail.com>
2019-02-19 19:11:34 -0800

    Create Task1.ps1
    A department that deals with 2000+ computers needed to sign in to each computer and remove two files. I created a script that can delete the two files from each of the computers attached to dhcp server for each group of computers. It will save that department hours of doing a repetitive task.

---
+++ Task1.ps1
@@ -0,0 +1,29 @@
+$Computers = Get-Content C:\Powershell\Store0726pos_Registers.txt
+$Creds1 = Get-Credential -Credential Creds1
+$Creds2 = Get-Credential -Credential Creds2
+
+foreach ($Computer in $Computers) {
+    Write-Host " Working with $Computer." -ForegroundColor Blue -BackgroundColor Yellow
+    Write-Output ""
+    
+    if ($Register -like 'CompLoc*D'){
+        Write-Host "  This is a Computer." -BackgroundColor White -ForegroundColor DarkRed
+        $Creds = $Creds2
+    } else {
+        Write-Host " This is a Laptop." -BackgroundColor White -ForegroundColor Blue
+        $Creds = $Creds1
+        }
+
+    New-PSDrive -Name H -Root \\$Computer\c$ -PSProvider FileSystem -Credential $Creds -OutVariable hDriveMapped | Out-Null
+    if ($hDriveMapped.root -eq "\\$Register\c$") {
+            Write-Host " $Computer's drive was mapped successfully" -ForegroundColor Green
+        }
+<#
+    Dir 'H:\Program files\Folder\SubFolder' | Where Name -eq File1 | Remove-Item
+    Dir 'H:\Program files\Folder\SubFolder' | Where Name -eq File2 | Remove-Item
+#>      
+    # removing drive
+    Write-Host "  Removing the mapped drive." -ForegroundColor Yellow
+    Remove-PSDrive H -Force
+    
+}
