0618ebb7773e7e2cff90dacd05aaea0711d8efce
Paul <ansibleoverride@gmail.com>
2020-08-18 23:30:53 -0700

    Create Get-SM.CitrixActiveConnections.ps1

---
+++ Get-SM.CitrixActiveConnections.ps1
@@ -0,0 +1,24 @@
+Function Get-SM.CitrixActiveConnections {
+    [Cmdletbinding()]
+    Param([Switch]$ShowConnectionsByServer
+    )
+    if ($ShowConnectionsByServer) {
+        Get-BrokerSession -MaxRecordCount 5000 | Where SessionState -eq Active -OutVariable BrokerSessions | Group DNSName |
+                            Select @{n='CitrixHost';e={$_.Name}},@{n='Sessions';e={
+                                Foreach ($Server in $_.Name) {$CheckIfInMaintenaceMode = Get-BrokerMachine "SM\$($Server.Split('.sm.lan')[0])" |
+                                    Select InMaintenanceMode
+                                    if ($CheckIfInMaintenaceMode.InMaintanceMode -eq $True) {
+                                    "InMaintenanceMode"
+                                    } else {$_.Count}}
+                            }}
+        } Else {
+            Get-BrokerSession -MaxRecordCount 5000 | Where SessionState -eq Active | Group 'DesktopGroupName' |
+                                Select @{n='DeliveryGroup';e={$_.Name}}, @{n='Sessions';e={$_.Count}}
+
+            Write-Output ""; $Total = $Null 
+            Get-BrokerSession -MaxRecordCount 5000 | Where SessionState -eq Active | Group 'DesktopGroupName' | 
+                                Select @{n='DeliveryGroup';e={$_.Name}}, @{n='Sessions';e={$_.Count -as [int]}} |
+                                    Foreach {$total += $_.Sessions}; $total |
+                                        Select @{n='Total Active Citrix Connections';e={$total}} | FL
+        }
+}
